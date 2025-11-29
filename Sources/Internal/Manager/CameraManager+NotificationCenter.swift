4937bd48a270c7968238c43364d8c48261a1114a
Pablo Butron <65462352+PabloButron@users.noreply.github.com>
2025-11-29 10:14:41 -0400

    Implement resumeSession on app foreground notification
    Added observer for app entering foreground to resume camera session.

--- Sources/Internal/Manager/CameraManager+NotificationCenter.swift
+++ Sources/Internal/Manager/CameraManager+NotificationCenter.swift
@@ -10,6 +10,7 @@
 
 
 import Foundation
+import UIKit
 
 @MainActor class CameraManagerNotificationCenter {
     private(set) var parent: CameraManager!
@@ -20,8 +21,20 @@ extension CameraManagerNotificationCenter {
     func setup(parent: CameraManager) {
         self.parent = parent
         NotificationCenter.default.addObserver(self, selector: #selector(handleSessionWasInterrupted), name: .AVCaptureSessionWasInterrupted, object: parent.captureSession)
+        NotificationCenter.default.addObserver(self, selector: #selector(resumeSession), name: UIApplication.willEnterForegroundNotification, object: nil)
+
+    }
+    
+    @objc private func resumeSession() {
+        let session = parent.captureSession
+        DispatchQueue.global(qos: .userInitiated).async {
+            if !session.isRunning {
+                session.startRunning() // Restart session when app enters foreground
+            }
+        }
     }
 }
+
 private extension CameraManagerNotificationCenter {
     @objc func handleSessionWasInterrupted() {
         parent.attributes.lightMode = .off
